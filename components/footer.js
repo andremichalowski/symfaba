43c2c320c3694678deaca1d48c5fb3306df1d59e
amichalo421 <andre.michalowski@gmail.com>
2021-04-08 19:15:14 -0400

    Footer update + style sheet

---
+++ components/footer.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Footer() {
+  return (
+    <div >
+      <footer className={styles.footer}>
+        <p>Test footer</p>
+        
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
