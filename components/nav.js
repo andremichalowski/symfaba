e2bb0dbf5ec8094f190601a5936fd0c6b06bb516
amichalo421 <andre.michalowski@gmail.com>
2021-04-13 18:53:34 -0400

    Nav image added

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,21 @@
 import Link from 'next/link'
-import logo1 from '../wireframes_and_images/logo1.jpg'
+import Image from 'next/image'
+import logo1 from '../public/logo1.jpg'
 
 export default function Nav() {
   return (
     <div >
-      <nav >
-        <img src={logo1} />
+      <nav style={{"margin": "20px"}}>
+        <div class="image_container">
+          <Image 
+            src={logo1}
+            alt="Picture of the author"
+            width={100}
+            height={150}
+          />
+        </div>
         <br/>
-        <Link href="/product"><a>Product</a></Link>
+        <Link href="/store_pages/product"><a>Products</a></Link>
         <br/>
         <Link href="/accessory_pages/articles"><a>Articles</a></Link>
         <br/>
@@ -15,6 +23,6 @@ export default function Nav() {
         <br/>
         <Link href="/accessory_pages/blog"><a>Blog</a></Link>
       </nav>
-    </div>
+    </div>  
   )
 }
