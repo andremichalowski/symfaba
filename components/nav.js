0fddeac1a3b31100ed606097dc341a816f0dbe63
amichalo421 <andre.michalowski@gmail.com>
2021-04-13 17:55:56 -0400

    Updating nav with image

--- components/nav.js
+++ components/nav.js
@@ -1,12 +1,18 @@
 import Link from 'next/link'
+import logo1 from '../wireframes_and_images/logo1.jpg'
 
 export default function Nav() {
   return (
     <div >
       <nav >
+        <img src={logo1} />
+        <br/>
         <Link href="/product"><a>Product</a></Link>
+        <br/>
         <Link href="/accessory_pages/articles"><a>Articles</a></Link>
+        <br/>
         <Link href="/accessory_pages/about"><a>About</a></Link>
+        <br/>
         <Link href="/accessory_pages/blog"><a>Blog</a></Link>
       </nav>
     </div>
