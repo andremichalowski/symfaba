cf81ee1638e45aa927fad1983ed463fed1945a6b
amichalo421 <andre.michalowski@gmail.com>
2021-04-13 11:31:49 -0400

    Removing old antD material

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,10 @@
-// import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
-// import Nav from '../components/nav'
-// // import Sider from '../components/antdTest'
-// import Footer from '../components/footer'
-
-// export default function Home() {
-//   return (
-//     <div className={styles.container}>
-//       <Head>
-//         <title>Create Next App</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       <Nav /> 
-//       {/* <Sider /> */}
-
-//       <main className={styles.main}>
-//         <h1>Home Page</h1>
-        
-//       </main>
-//       <Footer />
-//     </div>
-//   )
-// }
-
-import React from 'react';
-import reactDOM, {render} from 'react-dom';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Nav from '../components/nav'
 // import Sider from '../components/antdTest'
 import Footer from '../components/footer'
 
-export default function App() {
-
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -50,7 +21,7 @@ export default function App() {
       </main>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-// reactDOM.render(<App />, document.getElementById('__next'));
+
