aba6094c02c6a51187d747bc87bef44d79779295
amichalo421 <andre.michalowski@gmail.com>
2021-04-13 17:50:42 -0400

    Adding nav and footer components to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Nav from '../components/nav'
+import Footer from '../components/footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,19 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
-import Nav from '../components/nav'
-import Footer from '../components/footer'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-
-      <Nav /> 
-
-
+      
       <main className={styles.main}>
         <h1>Home Page</h1>
-        
       </main>
 
-      <Footer />
-    
     </div>
   )
 }
