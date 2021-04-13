4026beeafd9daa1ad26919d6dff0a51387731862
amichalo421 <andre.michalowski@gmail.com>
2021-04-13 15:24:04 -0400

    Update css imports for SASS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import Nav from '../components/nav'
 // import Sider from '../components/antdTest'
 import Footer from '../components/footer'
