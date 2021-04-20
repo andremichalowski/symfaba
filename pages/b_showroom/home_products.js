2883d2ddb41b33cf7fb6fcc4a6b1d25d921a07f0
amichalo421 <andre.michalowski@gmail.com>
2021-04-20 19:45:26 -0400

    Pages re-organized: 1. b_showroom

--- pages/b_showroom/home_products.js
+++ pages/b_showroom/home_products.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.scss'
-import ProductCard from '../components/product_card'
+import ProductCard from '../../components/product_card'
 import axios from 'axios'
 
 const Products = [
